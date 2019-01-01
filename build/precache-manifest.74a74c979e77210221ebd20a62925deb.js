054e5df5fff10ae34d4fa2599a5e7eaf74b56caf
Paul Tsai <tahsdj@gmail.com>
2019-01-01 14:59:26 +0800

    build up test

---
+++ build/precache-manifest.74a74c979e77210221ebd20a62925deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3ed52addb3186c54ec9",
+    "url": "/static/js/main.d3ed52ad.chunk.js"
+  },
+  {
+    "revision": "c331356755a079774c31",
+    "url": "/static/js/1.c3313567.chunk.js"
+  },
+  {
+    "revision": "d3ed52addb3186c54ec9",
+    "url": "/static/css/main.2742d2f2.chunk.css"
+  },
+  {
+    "revision": "0b9a24c4f454d327cb7c1f5a66805c8c",
+    "url": "/index.html"
+  }
+];
